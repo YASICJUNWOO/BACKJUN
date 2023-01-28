e9236a1da5fa33f7218fd68f1703b7626edf08b1
YASICJUNWOO <99794552+YASICJUNWOO@users.noreply.github.com>
2023-01-28 23:59:16 +0900

    [Bronze V] Title: 두 수 비교하기, Time: 212 ms, Memory: 17700 KB -BaekjoonHub

---
+++ 백준/Bronze/1330. 두 수 비교하기/두 수 비교하기.java
@@ -0,0 +1,23 @@
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        int a, b;
+
+        Scanner sc = new Scanner(System.in);
+        a = sc.nextInt();
+        b = sc.nextInt();
+
+        if(a>b){
+            System.out.println('>');
+        }else if(a==b)
+        {
+            System.out.println("==");
+        }else{
+            System.out.println('<');
+        }
+    }
+}
+
